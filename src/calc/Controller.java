05dc77d2614588717b078145afa1282b864c8024
JeffDziad <jeffdziad@gmail.com>
2021-07-17 03:59:39 -0500

    Added operators to GUI - Added simple display of numbers and operators to screen.

--- src/calc/Controller.java
+++ src/calc/Controller.java
@@ -1,64 +1,117 @@
 package calc;
 
+import com.sun.javafx.image.IntPixelGetter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
     @FXML
     private TextArea myScreen;
-    private String entries = "";
+    @FXML
+    private Button plus;
+    @FXML
+    private Button minus;
+    @FXML
+    private Button divide;
+    @FXML
+    private Button multiply;
+
+    private List<String> entries = new ArrayList<>();
+
+    private double solution = 0;
 
     public void one(ActionEvent e) {
-        entries += "1";
-        myScreen.setText(entries);
+        toEntryScreen("1");
     }
 
     public void two(ActionEvent e) {
-        entries += "2";
-        myScreen.setText(entries);
+        toEntryScreen("2");
     }
 
     public void three(ActionEvent e) {
-        entries += "3";
-        myScreen.setText(entries);
+        toEntryScreen("3");
     }
 
-
     public  void four (ActionEvent e) {
-        entries += "4";
-        myScreen.setText(entries);
+        toEntryScreen("4");
     }
 
     public void five(ActionEvent e) {
-        entries += "5";
-        myScreen.setText(entries);
+        toEntryScreen("5");
     }
 
     public void six(ActionEvent e) {
-        entries += "6";
-        myScreen.setText(entries);
+        toEntryScreen("6");
     }
 
     public void seven(ActionEvent e) {
-        entries += "7";
-        myScreen.setText(entries);
+        toEntryScreen("7");
     }
 
     public void eight(ActionEvent e) {
-        entries += "8";
-        myScreen.setText(entries);
+        toEntryScreen("8");
     }
 
     public void nine(ActionEvent e) {
-        entries += "9";
-        myScreen.setText(entries);
+        toEntryScreen("9");
     }
 
     public void zero(ActionEvent e) {
-        entries += "0";
-        myScreen.setText(entries);
+        toEntryScreen("0");
+    }
+
+    public void plus(ActionEvent actionEvent) {
+        toEntryScreen("+");
+    }
+
+    public void minus(ActionEvent actionEvent) {
+        toEntryScreen("-");
+    }
+
+    public void divide(ActionEvent actionEvent) {
+        toEntryScreen("/");
+    }
+
+    public void multiply(ActionEvent actionEvent) {
+        toEntryScreen("*");
+    }
+
+    public void point(ActionEvent actionEvent) {
+        toEntryScreen(".");
+    }
+
+    public void rParenth(ActionEvent actionEvent) {
+        toEntryScreen(")");
+    }
+
+    public void lParenth(ActionEvent actionEvent) {
+        toEntryScreen("(");
+    }
+
+    public void toEntryScreen(String entry) {
+        entries.add(entry);
+        myScreen.setText("");
+        StringBuilder output = new StringBuilder();
+        for(String i: entries) {
+            output.append(i);
+        }
+        myScreen.setText(String.valueOf(output));
+    }
+
+    public void equals(ActionEvent actionEvent) {
+        double op1, op2;
+        for(String entry: entries) {
+            try {
+                op1 = Integer.parseInt(entry);
+                //Below code - IS number;
+            }catch (Exception)
+        }
     }
 
 }
